46e1b73a1396aeec1bce9c91f30cdf8d9cab34b6
John Jacko <jljacko@outlook.com>
2021-02-09 13:02:12 -0600

    Prepared Resources Service for use with Oauth

---
+++ Resource-Service/src/main/java/com/trecapps/resources/controllers/UpdateController.java
@@ -0,0 +1,12 @@
+package com.trecapps.resources.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Update")
+public class UpdateController {
+
+
+
+}
